754eac17dd9caed9962a14d2ad6405531ae86634
Yu Seoyeon <kksy103@naver.com>
2025-05-31 14:44:16 +0900

    update 03-basic-projects

--- 03-basic-projects/main-5.py
+++ 03-basic-projects/main-5.py
@@ -15,13 +15,13 @@ while True:
     # print(f"코어: {cpu_core} 개" )
 
     cpu_p = psutil.cpu_percent(interval=1)
-    print(f'CPU사용량: {cpu_p}%')
+    print(f"CPU사용량: {cpu_p}%")
 
     memory = psutil.virtual_memory()
     # memory_total = round(memory.total / 1024**3)
-    memory_avail = round(memory.available/1024**3,1)
+    memory_avail = round(memory.available / 1024**3, 1)
     # print(f'메모리: {memory_total}GB' )
-    print(f'사용 가능한 메모리: {memory_avail}GB' )
+    print(f"사용 가능한 메모리: {memory_avail}GB")
 
     # disk = psutil.disk_partitions()
     # for p in disk:
@@ -31,13 +31,13 @@ while True:
     #     print(f'디스크크기: {disk_total}GB' )
 
     net = psutil.net_io_counters()
-    curr_sent = net.bytes_sent/1024**2
-    curr_recv = net.bytes_recv/1024**2
+    curr_sent = net.bytes_sent / 1024**2
+    curr_recv = net.bytes_recv / 1024**2
 
-    sent = round(curr_sent-prev_sent, 1)
-    recv = round(curr_recv-prev_recv, 1)
+    sent = round(curr_sent - prev_sent, 1)
+    recv = round(curr_recv - prev_recv, 1)
 
-    print(f'보내기: {sent}MB 받기: {recv}MB')
+    print(f"보내기: {sent}MB 받기: {recv}MB")
 
     prev_sent = curr_sent
     prev_recv = curr_recv
